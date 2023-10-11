4b8dfeeeeb12b2b6b959d8bd5842b33d55360ba1
junkdog505 <junkdog505@gmail.com>
2023-10-11 16:21:06 -0500

    Navigation Drawer (only view)

--- app/src/main/java/com/ucsm/proserge/LoginActivity.java
+++ app/src/main/java/com/ucsm/proserge/LoginActivity.java
@@ -2,7 +2,10 @@ package com.ucsm.proserge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Encuentra la referencia al botón por su ID
+        Button btnIngresar = findViewById(R.id.btnLogin); // Reemplaza "id_del_boton" con el ID real de tu botón
+
+        // Agrega un OnClickListener al botón
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Cuando se hace clic en el botón, inicia la NavigationDrawerActivity
+                Intent intent = new Intent(LoginActivity.this, NavigationDrawerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
