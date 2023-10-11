ffe3acdc1ea27890219564a91785662ed0b4ff7b
junkdog505 <junkdog505@gmail.com>
2023-10-11 18:48:33 -0500

    Home Fragment hecho

--- app/src/main/java/com/ucsm/proserge/LoginActivity.java
+++ app/src/main/java/com/ucsm/proserge/LoginActivity.java
@@ -1,23 +1,27 @@
 package com.ucsm.proserge;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // Encuentra la referencia al botón por su ID
-        Button btnIngresar = findViewById(R.id.btnLogin); // Reemplaza "id_del_boton" con el ID real de tu botón
+        Button btnIngresar = findViewById(R.id.btnLogin);
 
-        // Agrega un OnClickListener al botón
         btnIngresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/ucsm/proserge/NavigationDrawerActivity.java
+++ app/src/main/java/com/ucsm/proserge/NavigationDrawerActivity.java
@@ -1,14 +1,45 @@
 package com.ucsm.proserge;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+import com.ucsm.proserge.Fragments.HomeFragment;
+
 public class NavigationDrawerActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
+
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_drawer);
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+
+        //Fragment Home
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container, new HomeFragment());
+        fragmentTransaction.commit();
     }
 }
