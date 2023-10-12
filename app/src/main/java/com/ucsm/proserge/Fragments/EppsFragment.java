72be2506de55370ede9ef573643c9fceb7a3f9da
Chidow <joseph.ordonez@ucsm.edu.pe>
2023-10-12 16:15:25 -0500

    1: Botón Add y redirección de prueba

--- app/src/main/java/com/ucsm/proserge/Fragments/EppsFragment.java
+++ app/src/main/java/com/ucsm/proserge/Fragments/EppsFragment.java
@@ -3,11 +3,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.ucsm.proserge.R;
 
 public class EppsFragment extends Fragment {
@@ -15,8 +18,22 @@ public class EppsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.epps_fragment,container,false);
-        return view;
 
+        FloatingActionButton btnAddEpps = view.findViewById(R.id.floatingbuttonaddepps);
+        btnAddEpps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment homeFragment = new HomeFragment();
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.container, new HomeFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+
+
+        return view;
 
     }
 }
