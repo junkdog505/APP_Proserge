a2e8ee67cf7aea410851679dbce58904ecef940d
Chidow <joseph.ordonez@ucsm.edu.pe>
2023-10-13 19:43:50 -0500

    6: Consulta a BD EPPS sólo nombre (falta modificar para tipo y clase)

--- app/src/main/java/com/ucsm/proserge/Fragments/EppsFragment.java
+++ app/src/main/java/com/ucsm/proserge/Fragments/EppsFragment.java
@@ -1,4 +1,6 @@
 package com.ucsm.proserge.Fragments;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +15,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.ucsm.proserge.AdminSQLite;
 import com.ucsm.proserge.Epp;
 import com.ucsm.proserge.EppAdapter;
 import com.ucsm.proserge.R;
@@ -27,8 +30,10 @@ public class EppsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.epps_fragment,container,false);
+        AdminSQLite admin = new AdminSQLite(requireContext());
+        admin.copyDatabase();
 
+        View view = inflater.inflate(R.layout.epps_fragment,container,false);
         FloatingActionButton btnAddEpps = view.findViewById(R.id.floatingbuttonaddepps);
         btnAddEpps.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,27 +51,35 @@ public class EppsFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         // Obtén los datos de la base de datos y asigna a eppList
-        eppList = getGenericData();
+        eppList = getEppsFromDatabase();
         adapter = new EppAdapter(getContext(), eppList);
         recyclerView.setAdapter(adapter);
 
         return view;
     }
-    private List<Epp> getGenericData() {
-        List<Epp> dataList = new ArrayList<>();
+    private List<Epp> getEppsFromDatabase() {
+        List<Epp> eppList = new ArrayList<>();
+        AdminSQLite admin= new AdminSQLite(requireContext());
+        SQLiteDatabase db = admin.getReadableDatabase();
 
-        // Agrega algunos valores genéricos a la lista para probar
-        dataList.add(new Epp(1, "EPP 1"));
-        dataList.add(new Epp(2, "EPP 2"));
-        dataList.add(new Epp(3, "EPP 3"));
-        dataList.add(new Epp(4, "EPP 4"));
-        dataList.add(new Epp(5, "EPP 5"));
-        dataList.add(new Epp(6, "EPP 6"));
-        dataList.add(new Epp(7, "EPP 7"));
-        dataList.add(new Epp(8, "EPP 8"));
-        dataList.add(new Epp(9, "EPP 9"));
-        dataList.add(new Epp(10, "EPP 10"));
+        // Realiza una consulta en tu base de datos para obtener los registros
+        Cursor cursor = db.rawQuery("select * from epps", null);
 
-        return dataList;
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0); // Suponiendo que el ID está en la primera columna
+                String nombre = cursor.getString(1); // Suponiendo que el nombre está en la segunda columna
+
+                // Crea un objeto Epp y agrégalo a la lista
+                Epp epp = new Epp(id, nombre);
+                eppList.add(epp);
+            } while (cursor.moveToNext());
+        }
+
+        // Cierra el cursor y la base de datos
+        cursor.close();
+        db.close();
+
+        return eppList;
     }
 }
