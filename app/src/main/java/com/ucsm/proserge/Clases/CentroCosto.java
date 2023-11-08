1d7d91552d3550c778fd77dc03a4f455e3be590b
Chidow <joseph.ordonez@ucsm.edu.pe>
2023-11-08 18:50:23 -0500

    1: Clase CentroCosto

---
+++ app/src/main/java/com/ucsm/proserge/Clases/CentroCosto.java
@@ -0,0 +1,27 @@
+package com.ucsm.proserge.Clases;
+
+public class CentroCosto {
+    private String id, nombre;
+
+    public CentroCosto(String i, String n){
+        this.id = i;
+        this.nombre = n;
+    }
+
+    //Getters y Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
