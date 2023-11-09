9c22866180681b554c5589f0f18a0cfd64533552
Chidow <joseph.ordonez@ucsm.edu.pe>
2023-11-09 02:18:05 -0500

    5: Implementación de editar y eliminar, corrección de diseño. CRUD COMPLETO

--- app/src/main/java/com/ucsm/proserge/Adapters/TrabajadorAdapter.java
+++ app/src/main/java/com/ucsm/proserge/Adapters/TrabajadorAdapter.java
@@ -1,17 +1,22 @@
 package com.ucsm.proserge.Adapters;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.ucsm.proserge.AdminSQLite;
 import com.ucsm.proserge.CRUD.EditTrabajadorFragment;
 import com.ucsm.proserge.Clases.CentroCosto;
 import com.ucsm.proserge.Clases.Trabajador;
@@ -19,6 +24,7 @@ import com.ucsm.proserge.R;
 
 import org.w3c.dom.Text;
 
+import java.nio.BufferUnderflowException;
 import java.util.List;
 
 public class TrabajadorAdapter extends RecyclerView.Adapter<TrabajadorAdapter.ViewHolder> {
@@ -83,6 +89,60 @@ public class TrabajadorAdapter extends RecyclerView.Adapter<TrabajadorAdapter.Vi
                         //Redirecciona al fragmento EditCentroCostoFragment con valores de registro
                         FragmentManager fragmentManager = ((FragmentActivity) context).getSupportFragmentManager();
                         EditTrabajadorFragment editTrabajadorFragment = new EditTrabajadorFragment();
+
+                        //Envio de valores de registro al fragmento
+                        Bundle bundle = new Bundle();
+                        bundle.putString("dni", trabajadorToEdit.getDni());
+                        bundle.putString("nombres", trabajadorToEdit.getNombres());
+                        bundle.putString("apellidos", trabajadorToEdit.getApellidos());
+                        bundle.putString("cargo", trabajadorToEdit.getCargo());
+                        bundle.putString("contrato", trabajadorToEdit.getTipocontrato());
+                        editTrabajadorFragment.setArguments(bundle);
+
+                        fragmentManager.beginTransaction()
+                                .replace(R.id.container, editTrabajadorFragment)
+                                .addToBackStack(null)
+                                .commit();
+
+                    }
+                }
+            });
+
+            btnEliminar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+
+                    //Logica para eliminar
+                    Trabajador trabajadorToDelete = trabajadorList.get(position);
+
+                    AdminSQLite admin = new AdminSQLite(context);
+                    SQLiteDatabase db = admin.getWritableDatabase();
+
+                    String dni_to_delete = trabajadorToDelete.getDni();
+
+                    //Verificacion si hay registros relacionados (FK)
+                    int relatedRecordsCount = 0;
+                    Cursor cursor = db.rawQuery("SELECT COUNT(*) FROM OrdenTrabajo WHERE Id_Trabajador=?", new String[]{dni_to_delete});
+                    if (cursor.moveToFirst()) {
+                        relatedRecordsCount = cursor.getInt(0);
+                    }
+                    cursor.close();
+
+                    if (relatedRecordsCount > 0) {
+                        // Hay registros relacionados en otras tablas, muestra un mensaje de error
+                        Toast.makeText(context, "Este trabajador se encuentra en uso, no se puede eliminar", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // No hay registros relacionados, procede a eliminar el registro principal
+                        int changes = db.delete("Trabajador", "DNI=?", new String[]{dni_to_delete});
+                        db.close();
+                        if (changes == 1) {
+                            Toast.makeText(context, "Trabajador eliminado exitosamente", Toast.LENGTH_SHORT).show();
+                            trabajadorList.remove(position);
+                            notifyItemRemoved(position);
+                        } else {
+                            Toast.makeText(context, "El Trabajador no existe", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 }
             });
