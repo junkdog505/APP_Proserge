a86181d18b18eccdccb140d7522795642c898865
Chidow <joseph.ordonez@ucsm.edu.pe>
2023-11-08 23:42:43 -0500

    1: Clase Trabajador

---
+++ app/src/main/java/com/ucsm/proserge/Clases/Trabajador.java
@@ -0,0 +1,53 @@
+package com.ucsm.proserge.Clases;
+
+public class Trabajador {
+    private String dni, nombres, apellidos, cargo, tipocontrato;
+
+    public Trabajador(String d, String n, String a, String c, String t){
+        this.dni = d;
+        this.nombres = n;
+        this.apellidos = a;
+        this.cargo = c;
+        this.tipocontrato = t;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getTipocontrato() {
+        return tipocontrato;
+    }
+
+    public void setTipocontrato(String tipocontrato) {
+        this.tipocontrato = tipocontrato;
+    }
+}
