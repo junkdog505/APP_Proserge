fda7049948e6f65932727248366fb7895fefee55
Chidow <joseph.ordonez@ucsm.edu.pe>
2023-11-21 15:49:14 -0500

    7: item_detalle.xml para muestra de epps en detalle y abance de Adapter de Detalle Orden

--- app/src/main/java/com/ucsm/proserge/Adapters/DetalleOrdenTrabajoAdapter.java
+++ app/src/main/java/com/ucsm/proserge/Adapters/DetalleOrdenTrabajoAdapter.java
@@ -1,7 +1,31 @@
 package com.ucsm.proserge.Adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.ucsm.proserge.Clases.DetalleOrdenTrabajo;
+import com.ucsm.proserge.R;
+
+import java.util.List;
+
 public class DetalleOrdenTrabajoAdapter extends RecyclerView.Adapter<OrdenTrabajoAdapter.ViewHolder>{
-    private List<>
+    private List<DetalleOrdenTrabajo> detalleOrdenTrabajoList;
+    private Context context;
+
+    public DetalleOrdenTrabajoAdapter(Context context, List<DetalleOrdenTrabajo> detalleOrdenTrabajoList){
+        this.context = context;
+        this.detalleOrdenTrabajoList = detalleOrdenTrabajoList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_detalle, parent, false);
+        return new ViewHolder(view);
+    }
 }
