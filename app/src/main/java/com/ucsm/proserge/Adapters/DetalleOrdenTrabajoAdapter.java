131ab75eb120f2f7d3f8c0d4acd2a685894a7779
Chidow <joseph.ordonez@ucsm.edu.pe>
2023-11-21 20:27:11 -0500

    8: DetalleOrdenTrabajoAdapter

--- app/src/main/java/com/ucsm/proserge/Adapters/DetalleOrdenTrabajoAdapter.java
+++ app/src/main/java/com/ucsm/proserge/Adapters/DetalleOrdenTrabajoAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,7 +14,7 @@ import com.ucsm.proserge.R;
 
 import java.util.List;
 
-public class DetalleOrdenTrabajoAdapter extends RecyclerView.Adapter<OrdenTrabajoAdapter.ViewHolder>{
+public class DetalleOrdenTrabajoAdapter extends RecyclerView.Adapter<DetalleOrdenTrabajoAdapter.ViewHolder>{
     private List<DetalleOrdenTrabajo> detalleOrdenTrabajoList;
     private Context context;
 
@@ -27,5 +28,29 @@ public class DetalleOrdenTrabajoAdapter extends RecyclerView.Adapter<OrdenTrabaj
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_detalle, parent, false);
         return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position){
+        DetalleOrdenTrabajo detalleOrdenTrabajo = detalleOrdenTrabajoList.get(position);
+        holder.bind(detalleOrdenTrabajo);
+    }
+
+    @Override
+    public int getItemCount(){
+        return detalleOrdenTrabajoList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView textViewDetalleEpp;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            textViewDetalleEpp = itemView.findViewById(R.id.textViewDetalleNombreEpp);
+        }
+
+        public void bind(final DetalleOrdenTrabajo detalleOrdenTrabajo){
+            textViewDetalleEpp.setText(detalleOrdenTrabajo.getEpps_nombre());
+        }
     }
 }
